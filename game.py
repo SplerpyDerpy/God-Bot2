b6c9c89a20f6a90aa62369958a2e3ec2384e2540
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-10-16 18:26:16 +1100

    Update game.py

--- game.py
+++ game.py
@@ -14,13 +14,9 @@ async def on_ready():
     print("ready")
     await client.change_presence(game=Game(name="with Jesus"))
 
-
 @client.event
 async def on_message(message):
-    for server in client.servers:
-        for member in server.members:
-            print(member)
-   
+
     if message.content.upper().startswith('GO'):
         args = message.content.split(" ")
         print(args[1:])
@@ -28,21 +24,22 @@ async def on_message(message):
             await client.send_message(message.channel, "no, you")
     elif message.content.upper().startswith('-RULE34') or message.content.upper().startswith('-HENTAI'):
         await client.send_message(message.channel, "Thilan stop")
-    elif message.content.upper().startswith('@EVERYONE'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> why are you pinging everyone so pointlessly?" % (userID))
-    
     args = message.content.split(" ")	
     for word in args:
+        if word.upper() == '@EVERYONE':
+            userID = message.author.id
+            await client.send_message(message.channel, "<@%s> why are you pinging everyone so pointlessly?" % (userID))
         print(word)
         if word.upper() == ("FUCK") or word.upper() == ("SHIT"): 		
             if message.content.upper().startswith('SUCK MY DICK') or message.content.upper().startswith('FUCK YOU') or message.content.upper().startswith('GO FUCK YOURSELF'):
                 await client.send_message(message.channel, "no, you")
                 break
             else:
-                await client.send_message(message.channel, "keep this christian please")
+                async for msg in client.logs_from(message.channel, 1):
+                    await client.delete_message(msg)
                 break
 
 
 
 client.run("NTAxMjU4MDYyNjg2OTc4MDU4.DqWwgQ.YYnODCk_YRpErNojzSgLJI1OV_0")
+
