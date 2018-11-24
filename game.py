2521c14b46a3597ebff692a31533558e3be38ad2
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-11-24 14:16:06 +1100

    Update game.py

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     s = message.author.id
-    if s == '383381382870728705':
+    if s == '401940988106375179':
         await client.delete_message(message)
         for i in range(10):
             await client.send_message(message.author, "@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257@God of Emu#7257")
